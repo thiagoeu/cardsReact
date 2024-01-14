21ae189b7641bffa5451e02c0336389a3182ff8c
thiagoeu <araujo.thiago1051@gmail.com>
2024-01-14 16:51:48 -0300

    feat:: cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+import {loadPosts} from './utilitis/load-posts.js'
+import { Posts } from './components/Post/index.jsx';
+
+export default class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+
+    this.setState({ posts: postsAndPhotos })
+  }
+
+  render() {
+    const { posts } = this.state
+    return (
+
+      <section className='container'>
+        <Posts posts={posts} />
+      </section>
+
+
+    )
+  }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//   <div className='App'>
+//     <Job salary={2000} position ="senior" company ="amazon"></Job>
+//     <Users name="thiago" age = {10+10} ></Users>
+//     <Users name="thiago" age = {10+10} ></Users>
+//   </div>
+//   )
+// }
+
+
+// const Users = (props) =>{
+//   return(
+//     <>
+//       <h1> {props.name} </h1>
+//       <h1> {props.age} </h1>
+//     </>
+//   )
+// }
+
+
+// const Job = (props) =>{
+//   return(
+//     <>
+//       <h1> {props.salary} </h1>
+//       <h1> {props.position} </h1>
+//       <h1> {props.company} </h1>
+
+//     </>
+//   )
+// }
+
+
+// export default App;
---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+export const PostCard = ({title, cover, body, id}) =>{
+    return (
+        <div className='post'>
+                      <img src={cover} alt={title}></img>
+                      <div className='post-content'>
+                        <h2>{title}</h2> 
+                        <p>{body}</p>
+                      </div>
+         </div>
+    )
+}
+
